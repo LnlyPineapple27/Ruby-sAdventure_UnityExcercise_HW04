c35594abbd14910890c60fac9f6ccbb9d8516532
LnlyPineapple27 <47707507+LnlyPineapple27@users.noreply.github.com>
2021-12-19 16:08:34 +0700

    Add new InputSystem

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -3,7 +3,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class RubyController : MonoBehaviour
 { 
     public float speed = 3.0f;
@@ -31,32 +31,25 @@ public class RubyController : MonoBehaviour
     AudioSource audioSource;
     public AudioClip throwSound, hitSound;
     // Start is called before the first frame update
+    RubyInputActions inputActions;
+    Vector2 currentInput;
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         audioSource= GetComponent<AudioSource>();
         currentHealth = maxHealth;
+        // ==input==
+        inputActions = new RubyInputActions();
+        inputActions.Ruby.Enable();
+        inputActions.Ruby.Attack.performed += LaunchCog;
+        inputActions.Ruby.Movement.performed += OnMovement;
+        inputActions.Ruby.Movement.canceled += OnMovement;
     }
 
     // Update is called once per frame
     void Update()
-    {
-        horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
-        
-        Vector2 move = new Vector2(horizontal, vertical);
-        
-        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
-        {
-            lookDirection.Set(move.x, move.y);
-            lookDirection.Normalize();
-        }
-        
-        animator.SetFloat("Look X", lookDirection.x);
-        animator.SetFloat("Look Y", lookDirection.y);
-        animator.SetFloat("Speed", move.magnitude);
-        
+    { 
         if (isInvincible)
         {
             invincibleTimer -= Time.deltaTime;
@@ -64,10 +57,10 @@ public class RubyController : MonoBehaviour
                 isInvincible = false;
         }
         
-        if(Input.GetKeyDown(KeyCode.C))
-        {
-            LaunchCog();
-        }
+        // if(Input.GetKeyDown(KeyCode.C))
+        // {
+        //     LaunchCog();
+        // }
         if (Input.GetKeyDown(KeyCode.X))
         {
             RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
@@ -85,10 +78,25 @@ public class RubyController : MonoBehaviour
     void FixedUpdate()
     {
         Vector2 position = rigidbody2d.position;
-        position.x = position.x + speed * horizontal * Time.deltaTime;
-        position.y = position.y + speed * vertical * Time.deltaTime;
+        position = position + speed * currentInput * Time.deltaTime;
 
-        rigidbody2d.MovePosition(position);
+        rigidbody2d.MovePosition(position); 
+        //================Movement=====================
+        // horizontal = Input.GetAxis("Horizontal");
+        // vertical = Input.GetAxis("Vertical");
+        
+        Vector2 move = new Vector2(currentInput.x, currentInput.y);
+        
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
     }
 
     public void ChangeHealth(int amount)
@@ -113,13 +121,22 @@ public class RubyController : MonoBehaviour
         UIHealthBar.instance.SetValue(currentHealth / (float)maxHealth);
     }
     
-    void LaunchCog()
-    {
-        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
-        Projectile projectile = projectileObject.GetComponent<Projectile>();
-        projectile.Launch(lookDirection, 300);
-        audioSource.PlayOneShot(throwSound);
-        animator.SetTrigger("Launch");
+    void LaunchCog(InputAction.CallbackContext context){
+        if(context.performed){
+            GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+            Projectile projectile = projectileObject.GetComponent<Projectile>();
+            projectile.Launch(lookDirection, 300);
+            audioSource.PlayOneShot(throwSound);
+            animator.SetTrigger("Launch");
+        }
+    }
+    void OnMovement(InputAction.CallbackContext context){
+        if(context.performed){
+            currentInput = context.ReadValue<Vector2>();
+        }
+        if(context.canceled){
+            currentInput = Vector2.zero;
+        }
     }
     public void PlaySound(AudioClip clip)
     {
